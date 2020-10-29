23e969ddcd21023c079dee052f4c9e0188a8f411
jtreeves <jackson.t.reeves@gmail.com>
2020-10-29 16:43:30 -0400

    [promises] Create resolves, rejects, and conditional promises

--- app.js
+++ app.js
@@ -37,23 +37,23 @@
 // });
 
 // async await example:
-async function exercise2() {
-    // Initial log:
-    console.log('I should be before the cats data');
-    // First set of awaits:
-    const res1 = await fetch('http://www.reddit.com/search.json?q=cats+nsfw:no');
-    const json1 = await res1.json();
-    console.log(json1);
-    // Middle log:
-    console.log('I should be between the cats and dogs');
-    // Second set of awaits:
-    const res2 = await fetch('http://www.reddit.com/search.json?q=dogs+nsfw:no');
-    const json2 = await res2.json();
-    console.log(json2);
-    // Final log:
-    console.log('I should be last');
-};
-exercise2();
+// async function exercise2() {
+//     // Initial log:
+//     console.log('I should be before the cats data');
+//     // First set of awaits:
+//     const res1 = await fetch('http://www.reddit.com/search.json?q=cats+nsfw:no');
+//     const json1 = await res1.json();
+//     console.log(json1);
+//     // Middle log:
+//     console.log('I should be between the cats and dogs');
+//     // Second set of awaits:
+//     const res2 = await fetch('http://www.reddit.com/search.json?q=dogs+nsfw:no');
+//     const json2 = await res2.json();
+//     console.log(json2);
+//     // Final log:
+//     console.log('I should be last');
+// };
+// exercise2();
 
 // original snippet:
 // console.log('I should be before the cats data');
@@ -63,3 +63,31 @@ exercise2();
 // fetch('http://www.reddit.com/search.json?q=dogs+nsfw:no')
 // .then((res) => res.json()).then((json) => console.log(json))
 // console.log('I should be last');
+
+// Promises:
+// Boilerplate:
+// const myPromise = new Promise(function(resolve, reject) {} )
+// const successfulPromise = new Promise(function(resolve, reject) {
+//     setTimout(resolve, 3000);
+// });
+// console.log(successfulPromise);
+// const unsuccessfulPromise = new Promise(function(resolve, reject) {
+//     setTimeout(reject, 3000);
+// });
+// console.log(unsuccessfulPromise);
+
+// setTimeout(function() {
+//     console.log(successfulPromise);
+//     console.log(unsuccessfulPromise);
+// }, 5000);
+
+// conditional promise
+
+const conditionalPromise = new Promise(function(resolve, reject) {
+    if (2**3 === 3**2) {
+        reject();
+    } else {
+        resolve();
+    }
+})
+console.log(conditionalPromise);
